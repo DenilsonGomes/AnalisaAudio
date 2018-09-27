7d2f1b23d59638208726a42a380b64d3c6ee3d1d
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-09-27 16:12:46 -0300

    Questao finalizada

---
+++ questao10.m
@@ -0,0 +1,6 @@
+% Autor: Denilson Gomes Vaz da Silva
+% Departamento de Engenharia de Computação
+% Processamento Digital de Sinais
+% O procedimento implementado se aplica a qualquer sinal de audio, logo
+% para aplica-lo ao audio desejado basta descomentar o seu carregamento
+% (load nome_arquivo_de_audio) e comentar os demais
