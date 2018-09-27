5bbb430e0986e169e30d8e323c0a95e938b0d4da
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-09-27 15:33:35 -0300

    Codigo finalizado

---
+++ questao5.m
@@ -0,0 +1,6 @@
+% Autor: Denilson Gomes Vaz da Silva
+% Departamento de Engenharia de Computação
+% Processamento Digital de Sinais
+% Script para visualizar diagrama de polos e zeros
+
+zplane(B,A) %plota filtro passa-faixa no plano complexo
