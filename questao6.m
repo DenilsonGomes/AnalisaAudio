39382ad4aae9c617f455bb92dd459fabc6025cdb
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-09-27 16:03:09 -0300

    Possivel codigo (ainda sem certeza) finalizado

---
+++ questao6.m
@@ -0,0 +1,7 @@
+% Autor: Denilson Gomes Vaz da Silva
+% Departamento de Engenharia de Computação
+% Processamento Digital de Sinais
+% Script para visualizar filtro com fvtool
+
+fvtool(W,H)
+fvtool(B,A)
