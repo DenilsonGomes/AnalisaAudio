e2b09c2cd05d96d3126cfdea71eba87fe90a00f3
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-09-27 14:32:16 -0300

    Codigo finalizado

--- questao1.m
+++ questao1.m
@@ -1,8 +1,18 @@
 % Autor: Denilson Gomes Vaz da Silva
 % Departamento de Engenharia de Computação
+%Processamento Digital de Sinais
 % Script para carregar e executar o audio "train.mat"
 
 load train
 
 %reproduz o audio na frequencia normal
+sound(y,Fs)
+
+%recebe o multiplo da frequencia normal
+m = input('Digite o multiplo da frequencia que deseja ouvir o audio: ');
+y = y(:,1);
+y = y(1:m:end); %muda a amostragem do sinal
+Fs = Fs/m; %muda a frequencia
+
+%reproduz o audio na nova frequencia
 sound(y,Fs)
